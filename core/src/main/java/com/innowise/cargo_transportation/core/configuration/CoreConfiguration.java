2508c94add962e9dea667a9b7c91a1f4c766ded6
Alex Che <sat.git333@mail.ru>
2022-01-14 16:04:13 +0300

    CTB-1: init project

---
+++ core/src/main/java/com/innowise/cargo_transportation/core/configuration/CoreConfiguration.java
@@ -0,0 +1,9 @@
+package com.innowise.cargo_transportation.core.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.innowise.cargo_transportation.core")
+public class CoreConfiguration {
+}
