72cbe49600bbb2332ea55202be124cfd59037171
Alex Che <sat.git333@mail.ru>
2022-01-14 12:12:49 +0300

    Initial commit

---
+++ src/main/java/com/innowise/cargoTransportation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.innowise.cargoTransportation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CargoTransportationApplication.class);
+	}
+
+}
