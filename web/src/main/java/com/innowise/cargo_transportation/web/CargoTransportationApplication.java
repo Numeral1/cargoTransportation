c3efabe7dfedd547c8b53845654f15d52f845e9f
Stas <stanislove.korol@gmail.com>
2022-01-17 23:27:21 +0300

    CTB-2: remove unused imports

--- web/src/main/java/com/innowise/cargo_transportation/web/CargoTransportationApplication.java
+++ web/src/main/java/com/innowise/cargo_transportation/web/CargoTransportationApplication.java
@@ -2,11 +2,8 @@ package com.innowise.cargo_transportation.web;
 
 
 import com.innowise.cargo_transportation.core.configuration.CoreConfiguration;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
