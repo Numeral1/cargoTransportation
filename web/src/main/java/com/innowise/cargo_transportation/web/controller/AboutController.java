546c901f5ca81a2a1a7d1de1af5c0f81c5ac42f4
Stas <stanislove.korol@gmail.com>
2022-01-17 22:15:58 +0300

    CTB-2: replace RequestMapping by GetMapping

--- web/src/main/java/com/innowise/cargo_transportation/web/controller/AboutController.java
+++ web/src/main/java/com/innowise/cargo_transportation/web/controller/AboutController.java
@@ -4,8 +4,8 @@ package com.innowise.cargo_transportation.web.controller;
 import com.innowise.cargo_transportation.core.configuration.AboutInfoComponent;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +15,7 @@ public class AboutController {
     @NonNull
     private AboutInfoComponent aboutInfoComponent;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public AboutInfoComponent getAbout() {
         return aboutInfoComponent;
     }
