77b92979926ce229f8c504dcb7db7e0e159a0d1d
Stas <stanislove.korol@gmail.com>
2022-01-18 12:42:12 +0300

    CTB-3: add CORS policy

--- web/src/main/java/com/innowise/cargo_transportation/web/controller/AboutController.java
+++ web/src/main/java/com/innowise/cargo_transportation/web/controller/AboutController.java
@@ -4,9 +4,7 @@ package com.innowise.cargo_transportation.web.controller;
 import com.innowise.cargo_transportation.core.configuration.AboutInfoComponent;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/about")
